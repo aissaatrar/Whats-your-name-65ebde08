94c346d9c8b48dcb159f6372c407046e84ca057a
ilias atrar <23261@ma-web.nl>
2019-11-27 10:06:29 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,15 @@
+
+<?php
+
+
+$naam;
+$achternaam;
+echo 'Wat is je voornaam? ' . "\n";
+$naam = readline();
+echo 'Wat is je achternaam? ' . "\n";
+$achternaam = readline();
+echo "Jouw naam is: " . $naam .  ' ' . $achternaam . '  ';
+
+
+
+ ?>
